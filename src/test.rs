3d601a7b832c775fd8d84d198c7e114f5406cdcd
Rasavejo <rasavejo@pop-os.home>
2026-02-18 10:43:23 +0100

    added simple consistency tests

--- src/test.rs
+++ src/test.rs
@@ -15,6 +15,49 @@ mod tests {
                 simulate(machine.to_str().unwrap(), task.to_str().unwrap());
             }
         }
-
     }
+
+    #[test]
+    fn start_time_delay() {
+        let machines = "examples/machines";
+        for machine in fs::read_dir(machines).unwrap(){
+            let machine = machine.unwrap().path();
+            assert!(
+                simulate(machine.to_str().unwrap(),"examples/tasks/one-task.json")
+                <
+                simulate(machine.to_str().unwrap(),"examples/tasks/one-task-delayed.json")
+            );
+        }
+    }
+
+
+    #[test]
+    fn mono_core() {
+        assert!(
+            simulate("examples/machines/mono-core.json","examples/tasks/two-parallel-task.json")
+            ==
+            simulate("examples/machines/mono-core.json","examples/tasks/two-sequential-task.json")
+        );
+        assert!(
+            simulate("examples/machines/mono-core.json","examples/tasks/one-task.json") * 2
+            ==
+            simulate("examples/machines/mono-core.json","examples/tasks/two-sequential-task.json")
+        );
+    }
+
+    
+    #[test]
+    fn multiple_tasks() {
+        assert!(
+            simulate("examples/machines/dual-core.json","examples/tasks/two-parallel-task.json")
+            <
+            simulate("examples/machines/dual-core.json","examples/tasks/two-sequential-task.json")
+        );
+        assert!(
+            simulate("examples/machines/mono-core.json","examples/tasks/two-sequential-task.json")
+            ==
+            simulate("examples/machines/dual-core.json","examples/tasks/two-sequential-task.json")
+        );
+    }
+
 }
